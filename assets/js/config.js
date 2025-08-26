5d36a6ad3799153a5f33268a361b684fedf3f6d3
Padu76 <andrea.padoan@gmail.com>
2025-08-26 11:28:53 +0200

    Add files via upload

---
+++ assets/js/config.js
@@ -0,0 +1,14 @@
+// config.js
+window.CONFIG = {
+  GOOGLE_API_KEY: "INSERISCI_LA_TUA_API_KEY",
+  GOOGLE_CLIENT_ID: "INSERISCI_LA_TUA_CLIENT_ID",
+  FIREBASE_CONFIG: {
+    apiKey: "INSERISCI_API_KEY",
+    authDomain: "tuo-progetto.firebaseapp.com",
+    projectId: "tuo-progetto",
+    storageBucket: "tuo-progetto.appspot.com",
+    messagingSenderId: "000000000000",
+    appId: "1:000000000000:web:xxxxxxxxxxxxxx"
+  },
+  STRIPE_PUBLIC_KEY: "INSERISCI_CHIAVE_STRIPE_PUBLIC"
+};
